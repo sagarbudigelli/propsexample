159d63b8e028b3967334665cd9e3c9c9e791b59d
SESHAPUSIVAKUMAR <123seshapu@gmail.com>
2022-11-30 16:59:10 +0530

    30-11-22

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+//import ClassComponent from'./ClassComponent'
+import PropsExample from './PropsExample'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function App(){
+	return(
+		<section>
+		<PropsExample name="sagar" age="20"/>
+			{/*
+			<p>good boy</p>
+			<h1>good one</h1>
+			<img src="https://www.deccanherald.com/sites/dh/files/styles/article_detail/public/article_images/2020/05/19/file74g16bzwmc819e5i5hwd-807658252-1552396810.jpg?itok=qp5SV2vb"/>
+			<ClassComponent/>*/}
+		</section>
+		)
 }
 
 export default App;
+
