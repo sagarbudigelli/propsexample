96c9c06a6d2a0266d3512d04338bb29fce1ecbaf
SESHAPUSIVAKUMAR <123seshapu@gmail.com>
2022-12-01 16:51:24 +0530

    events & states

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 //import ClassComponent from'./ClassComponent'
-import PropsExample from './PropsExample'
-
+//import PropsExample from './PropsExample'
+import FunctionProps from './FunctionProps'
+import StateExample from './StateExample'
+import Demo from './Demo'
+import Events from './Events'
 function App(){
 	return(
 		<section>
-		<PropsExample name="sagar" age="20"/>
-			{/*
+		<StateExample/>
+		<Demo price="1st"/>
+		<Events/>
+		<FunctionProps carname="rollsroyce" year="1998"/>
+		{/*<PropsExample name="sagar" age="20"/>
+			
 			<p>good boy</p>
 			<h1>good one</h1>
 			<img src="https://www.deccanherald.com/sites/dh/files/styles/article_detail/public/article_images/2020/05/19/file74g16bzwmc819e5i5hwd-807658252-1552396810.jpg?itok=qp5SV2vb"/>
---
+++ src/Demo.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+class Demo extends React.Component{
+	state={
+		s:this.props.price
+
+	}
+
+render(){
+	return(
+		<div>
+			<p>{this.state.s}</p>
+			<h1>{this.props.price}</h1>
+		</div>
+		)
+	}
+
+}
+export default Demo
